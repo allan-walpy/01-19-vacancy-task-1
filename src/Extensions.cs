364d735074e2a915470af7c3a091f624eb90192e
allan_walpy <allanwalpy@gmail.com>
2019-01-29 18:43:27 +0300

    #2 add MVC

---
+++ src/Extensions.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Server
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddAppMvc(this IServiceCollection services)
+            => services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .Services;
+
+    }
+}
