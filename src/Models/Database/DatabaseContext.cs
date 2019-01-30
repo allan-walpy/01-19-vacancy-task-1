bfb363e0300bcd2c1886bb7cb97da412e239c835
allan_walpy <allanwalpy@gmail.com>
2019-01-30 16:55:04 +0300

    #5 add database context

---
+++ src/Models/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Server.Models.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Vacancy> Vacancies { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+
+        public DatabaseContext(DbContextOptions options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
