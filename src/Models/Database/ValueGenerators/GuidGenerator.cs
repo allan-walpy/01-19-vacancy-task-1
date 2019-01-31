f02eccfce7a4ca30d610c8a70d20b3d4e47597e9
allan_walpy <allanwalpy@gmail.com>
2019-01-31 21:12:14 +0300

    #5 moved ValueGenerators to Database.ValueGenerators namespace

--- src/Models/Database/ValueGenerators/GuidGenerator.cs
+++ src/Models/Database/ValueGenerators/GuidGenerator.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.EntityFrameworkCore.ValueGeneration;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
-namespace App.Server.Models.Database
+namespace App.Server.Models.Database.ValueGenerators
 {
     public class GuidGenerator : ValueGenerator<string>
     {
