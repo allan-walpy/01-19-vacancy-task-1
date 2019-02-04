478df8fe97542db603f0c3e22f1251f3bd4f6469
allan_walpy <allanwalpy@gmail.com>
2019-02-04 22:09:30 +0300

    close #12 add scripts with workaround for client refuse connection

--- script/test.sh
+++ script/test.sh
@@ -3,4 +3,12 @@
 ## tests app;
 ##   must be launched from root repository folder;
 
-dotnet test --no-restore
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.GetAllTest" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.GetTest" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.AddTest" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.DeleteTest" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.UpdateTest.Valid200Title" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.UpdateTest.Valid200Description" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.UpdateTest.Valid200Salary" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.UpdateTest.Valid200ContactPhone" --verbosity n
+dotnet test --filter FullyQualifiedName~"App.Server.Test.Api.Vacancy.UpdateTest.Valid200ContactPerson" --verbosity n
