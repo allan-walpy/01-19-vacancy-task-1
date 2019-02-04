4b921ba336077b796d8d7c34ba983b749148b2ce
allan_walpy <allanwalpy@gmail.com>
2019-02-04 21:57:06 +0300

    #12 add Delete Test

---
+++ test/Api/Vacancy/DeleteTest.cs
@@ -0,0 +1,58 @@
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Xunit;
+
+using App.Server.Models.Responses;
+using App.Server.Test.Models;
+using App.Server.Test.Data;
+
+namespace App.Server.Test.Api.Vacancy
+{
+    public partial class DeleteTest : VacancyTestBase
+    {
+        public static BaseTheoryData<int, object> DatabaseItemsId
+            => new BaseTheoryData<int, object>(
+                Enumerable.Range(0, DefaultData.VacancyData.Count - 1));
+
+        public override HttpMethod Method => HttpMethod.Get;
+
+        public DeleteTest()
+            : base(DefaultData.VacancyData)
+        { }
+
+        protected override void AssertContentAs<T>(T expected, T actual)
+        {
+            var guid = expected as string;
+            var database = GetItems();
+            Assert.DoesNotContain(database, (vacancy) => vacancy.Id == guid);
+        }
+
+        [Theory]
+        [MemberData(nameof(DatabaseItemsId))]
+        public void Valid200(int id)
+        {
+            var guid = DatabaseGuids[id];
+
+            var expected = new HttpMessageModel
+            {
+                ApiCall = null,
+                StatusCode = StatusCodes.Status200OK,
+                Headers = null,
+                Data = guid,
+                BadModel = null
+            };
+
+            var request = new HttpMessageModel
+            {
+                ApiCall = null,
+                StatusCode = null,
+                Headers = null,
+                Data = null,
+                BadModel = null
+            };
+
+            AssertValidResponseByRequest<VacancyResponse>(request, expected, guid);
+        }
+    }
+}
